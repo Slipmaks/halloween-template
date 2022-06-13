fb16672dcc6a67b735c4e334ca1ae0bd720b5ab9
Slipmaks <slipmaks@gmail.com>
2022-06-13 10:36:27 +0300

    finish responsive, add scrollreveal

--- assets/js/main.js
+++ assets/js/main.js
@@ -88,3 +88,18 @@ function scrollUp() {
   else scrollUp.classList.remove("show-scroll");
 }
 window.addEventListener("scroll", scrollUp);
+
+// scroll reval animation
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+});
+
+sr.reveal(".home-swiper, .new-swiper, .newsletter__container");
+sr.reveal(".category__data, .trick__content, .footer__content", {
+  interval: 100,
+});
+sr.reveal(".about__data, .discount__img", { origin: "left" });
+sr.reveal(".about__img, .discount__data", { origin: "right" });
